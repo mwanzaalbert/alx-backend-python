b4debf51324f6baea64121a3b988ee8a7f4207b8
mwanzaalbert <user.email>
2024-10-15 15:27:52 +0300

    updated typed annotations

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -6,11 +6,10 @@ Created on Tue Oct 15 13:59:11 2024.
 @author: AM
 """
 
-
 import asyncio
 import time
 
-# Import async_generator from the previous task
+# Import async_comprehension from the previous task
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
@@ -20,22 +19,21 @@ async def measure_runtime() -> float:
 
     and measure the total runtime.
     """
-    start_time = time.perf_counter()  # Start the timer
+    start_time: float = time.perf_counter()  # Start the timer
 
     # Run 4 instances of async_comprehension in parallel using asyncio.gather
     await asyncio.gather(*(async_comprehension() for _ in range(4)))
 
-    end_time = time.perf_counter()  # End the timer
+    end_time: float = time.perf_counter()  # End the timer
 
-    total_runtime = end_time - start_time  # Calculate total runtime
+    total_runtime: float = end_time - start_time  # Calculate total runtime
     return total_runtime
 
 
-if __name__ == "__main__":
-    async def main():
-        """Entry Point."""
-        return await (measure_runtime())
+async def main() -> None:
+    """Entry Point."""
+    print(await measure_runtime())
 
-    print(
-        asyncio.run(main())
-    )
+
+if __name__ == "__main__":
+    asyncio.run(main())
