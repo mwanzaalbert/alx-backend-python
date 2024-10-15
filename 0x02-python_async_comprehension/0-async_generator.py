49798243656aeac559263051448f0049bfe6c703
mwanzaalbert <user.email>
2024-10-15 15:17:23 +0300

    corrected typed annotations

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -8,26 +8,27 @@ Created on Tue Oct 15 13:34:17 2024.
 
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import AsyncGenerator, List
 
 
 async def async_generator() -> AsyncGenerator[float, None]:
     """
     Asynchronously generates 10 random numbers between 0 and 10.
 
-    waiting 1 second between each.
+    It waits 1 second between each number.
     """
     for _ in range(10):
         await asyncio.sleep(1)  # Asynchronously wait for 1 second
         yield random.uniform(0, 10)  # Yield a random float between 0 and 10
 
 
-if __name__ == "__main__":
-    async def print_yielded_values():
-        """Execute Asynchronous Generator."""
-        result = []
-        async for i in async_generator():
-            result.append(i)
-        print(result)
+async def print_yielded_values() -> None:
+    """Collect and print the values yielded by the async_generator."""
+    result: List[float] = []
+    async for i in async_generator():
+        result.append(i)
+    print(result)
 
+
+if __name__ == "__main__":
     asyncio.run(print_yielded_values())
