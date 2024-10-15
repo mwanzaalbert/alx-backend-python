82131a572bb753a68fcd393389c2ddfda66ac21d
mwanzaalbert <user.email>
2024-10-15 14:20:41 +0300

    return annotation corrected to AsyncGenerator

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -8,10 +8,10 @@ Created on Tue Oct 15 13:34:17 2024.
 
 import asyncio
 import random
-from typing import Generator
+from typing import AsyncGenerator
 
 
-async def async_generator() -> Generator[float, None, None]:
+async def async_generator() -> AsyncGenerator[float, None]:
     """
     Asynchronously generates 10 random numbers between 0 and 10.
 
